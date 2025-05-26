a12bb00a6626e7eeb84122e5350670f88b6fd125
FredricLaestander <fredriclaestander@gmail.com>
2025-05-26 16:11:17 +0200

    create count component

---
+++ apps/frontend/src/components/Count.tsx
@@ -0,0 +1,7 @@
+export const Count = ({ value }: { value: string }) => {
+  return (
+    <span className="flex w-16 justify-center rounded-full bg-slate-900 px-2 py-0.5 font-bold">
+      {value}
+    </span>
+  )
+}
