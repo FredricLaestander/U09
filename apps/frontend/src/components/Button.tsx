7a9b3c4ce2650f69cc19df7bc81c9dce5c556d82
ell-ska <ella@skagerstrom.se>
2025-05-28 10:41:27 +0200

    feat: create Button

---
+++ apps/frontend/src/components/Button.tsx
@@ -0,0 +1,43 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import type { ElementType } from 'react'
+import type { Polymorphic } from '../types'
+import { cn } from '../utils/classname'
+
+const variants = cva(
+  'flex items-center justify-center gap-3 text-base font-bold whitespace-nowrap rounded-full transition cursor-pointer disabled:bg-slate-600 disabled:text-slate-800 disabled:cursor-auto',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-slate-800 text-slate-50 hover:bg-slate-950',
+        blue: 'bg-blue text-slate-800 hover:bg-blue-darker',
+        green: 'bg-green text-slate-800 hover:bg-green-darker',
+        red: 'bg-red text-slate-50 hover:bg-red-darker',
+      },
+      size: {
+        md: 'py-3 px-8',
+        sm: 'py-2 px-6',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      size: 'md',
+    },
+  },
+)
+
+export const Button = <Element extends ElementType = 'button'>({
+  as,
+  children,
+  variant,
+  size,
+  className,
+  ...rest
+}: Polymorphic<VariantProps<typeof variants>, Element>) => {
+  const Element = as || 'button'
+
+  return (
+    <Element className={cn(variants({ variant, size, className }))} {...rest}>
+      {children}
+    </Element>
+  )
+}
