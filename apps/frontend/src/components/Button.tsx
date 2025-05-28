2e9536b8c59c32d8baf6c6fff96679fa38a00dc5
ell-ska <ella@skagerstrom.se>
2025-05-28 14:40:42 +0200

    feat: create Button

--- apps/frontend/src/components/Button.tsx
+++ apps/frontend/src/components/Button.tsx
@@ -1,10 +1,11 @@
 import { cva, type VariantProps } from 'class-variance-authority'
+import type { LucideIcon } from 'lucide-react'
 import type { ElementType } from 'react'
 import type { Polymorphic } from '../types'
 import { cn } from '../utils/classname'
 
 const variants = cva(
-  'flex items-center justify-center gap-3 text-base font-bold whitespace-nowrap rounded-full transition cursor-pointer disabled:bg-slate-600 disabled:text-slate-800 disabled:cursor-auto',
+  'flex items-center justify-center text-base font-bold whitespace-nowrap rounded-full transition cursor-pointer disabled:bg-slate-600 disabled:text-slate-800 disabled:cursor-auto',
   {
     variants: {
       variant: {
@@ -14,8 +15,8 @@ const variants = cva(
         red: 'bg-red text-slate-50 hover:bg-red-darker',
       },
       size: {
-        md: 'py-3 px-8',
-        sm: 'py-2 px-6',
+        md: 'py-3 px-8 gap-3',
+        sm: 'py-2 px-6 gap-2',
       },
     },
     defaultVariants: {
@@ -28,15 +29,24 @@ const variants = cva(
 export const Button = <Element extends ElementType = 'button'>({
   as,
   children,
+  icon: Icon,
   variant,
   size,
   className,
   ...rest
-}: Polymorphic<VariantProps<typeof variants>, Element>) => {
+}: Polymorphic<
+  VariantProps<typeof variants> & { icon?: LucideIcon },
+  Element
+>) => {
   const Element = as || 'button'
 
   return (
     <Element className={cn(variants({ variant, size, className }))} {...rest}>
+      {Icon && (
+        <Icon
+          className={cn(size === 'md' && 'size-6', size === 'sm' && 'size-4')}
+        />
+      )}
       {children}
     </Element>
   )
