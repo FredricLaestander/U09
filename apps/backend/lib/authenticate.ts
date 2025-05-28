96dbf20a95621f4a6c09fde2fb25f4b64dc56aa3
ell-ska <ella@skagerstrom.se>
2025-05-28 14:26:54 +0200

    feat: authenticate in handle

---
+++ apps/backend/lib/authenticate.ts
@@ -0,0 +1,34 @@
+import type { Request } from 'express'
+import jwt from 'jsonwebtoken'
+import { z } from 'zod'
+import { prisma } from '../prisma/client'
+import { AuthError } from './error'
+import { validate } from './validate'
+
+export const authenticate = async (cookies: Request['cookies']) => {
+  const accessToken = validate(cookies['access-token'], z.string())
+
+  if (!accessToken) {
+    throw new AuthError('access token missing', 401)
+  }
+
+  let decodedToken
+
+  try {
+    decodedToken = jwt.verify(accessToken, Bun.env.ACCESS_TOKEN_SECRET!)
+  } catch (error) {
+    const message =
+      error instanceof Error ? error.message : 'invalid access token'
+    throw new AuthError(message, 401)
+  }
+
+  const { id } = validate(decodedToken, z.object({ id: z.string() }))
+
+  const user = await prisma.user.findUnique({ where: { id } })
+
+  if (!user) {
+    throw new AuthError('user does not exist', 401)
+  }
+
+  return id
+}
