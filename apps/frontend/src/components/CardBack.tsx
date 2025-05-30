55ece2349b53b11847e66953ee9a93ec310d036f
FredricLaestander <fredriclaestander@gmail.com>
2025-05-30 14:54:33 +0200

    feat: create cardback component

---
+++ apps/frontend/src/components/CardBack.tsx
@@ -0,0 +1,7 @@
+export const CardBack = () => {
+  return (
+    <div className="h-60 w-40 rounded-xl bg-slate-50 p-2 shadow-lg">
+      <img src="/pattern.jpg" alt="card back" className="rounded-sm" />
+    </div>
+  )
+}
