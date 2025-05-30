9235b63fc2b378d48484737d458bc1bb0187afeb
ell-ska <ella@skagerstrom.se>
2025-05-30 11:52:37 +0200

    feat: create Toaster

---
+++ apps/frontend/src/components/Toaster.tsx
@@ -0,0 +1,84 @@
+import * as Primitive from '@radix-ui/react-toast'
+import { AlertCircle, type LucideIcon } from 'lucide-react'
+import {
+  createContext,
+  use,
+  useState,
+  type Dispatch,
+  type ReactNode,
+  type SetStateAction,
+} from 'react'
+
+type Type = 'error'
+
+type TToast = {
+  id: string
+  description: string
+  type: Type
+}
+
+const ToastContext = createContext<{
+  toasts: TToast[]
+  setToasts: Dispatch<SetStateAction<TToast[]>>
+} | null>(null)
+
+export const ToastProvider = ({ children }: { children: ReactNode }) => {
+  const [toasts, setToasts] = useState<TToast[]>([])
+
+  return (
+    <ToastContext.Provider value={{ toasts, setToasts }}>
+      {children}
+      <Primitive.Provider>
+        {toasts.map((toast) => (
+          <Toast {...toast} key={toast.id} />
+        ))}
+        <Primitive.Viewport className="fixed top-12 right-4 left-4 flex flex-col gap-2 md:top-auto md:right-8 md:bottom-6 md:left-auto" />
+      </Primitive.Provider>
+    </ToastContext.Provider>
+  )
+}
+
+const iconMap: Record<Type, LucideIcon> = {
+  error: AlertCircle,
+}
+
+const Toast = ({ id, description, type }: TToast) => {
+  const context = use(ToastContext)
+  const Icon = iconMap[type]
+
+  const remove = () => {
+    context!.setToasts((prev) => prev.filter((toast) => toast.id !== id))
+  }
+
+  return (
+    <Primitive.Root
+      open
+      onOpenChange={() => {
+        remove()
+      }}
+      duration={3000}
+      className="flex items-center gap-2 rounded-lg border border-slate-900 bg-slate-800 px-4 py-3 shadow-lg md:max-w-sm md:min-w-xs"
+    >
+      <Icon className="text-red" />
+      <Primitive.Description className="text-sm">
+        {description}
+      </Primitive.Description>
+    </Primitive.Root>
+  )
+}
+
+export const useToast = () => {
+  const context = use(ToastContext)
+  if (!context) {
+    throw new Error('useToast must be used within a ToastProvider')
+  }
+
+  return {
+    error: (description: TToast['description']) => {
+      context.setToasts((prev) => [
+        ...prev,
+        { id: crypto.randomUUID(), description, type: 'error' },
+      ])
+    },
+  }
+}
--- apps/frontend/src/router.tsx
+++ apps/frontend/src/router.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router'
+import { Layout } from './layout'
 import { EditProfilePage } from './pages/EditProfile'
 import { GamePage } from './pages/Game'
 import { LandingPage } from './pages/Landing'
@@ -10,35 +11,41 @@ import { RegisterPage } from './pages/Register'
 
 export const router = createBrowserRouter([
   {
-    path: '/landing',
-    element: <LandingPage />,
-  },
-  {
     path: '/',
-    element: <MainMenuPage />,
-  },
-  {
-    path: '/game',
-    element: <GamePage />,
-  },
-  {
-    path: '/profile',
-    element: <ProfilePage />,
-  },
-  {
-    path: '/edit-profile',
-    element: <EditProfilePage />,
-  },
-  {
-    path: '/register',
-    element: <RegisterPage />,
-  },
-  {
-    path: '/personalize-account',
-    element: <PersonalizeAccountPage />,
-  },
-  {
-    path: '/log-in',
-    element: <LogInPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/landing',
+        element: <LandingPage />,
+      },
+      {
+        path: '/',
+        element: <MainMenuPage />,
+      },
+      {
+        path: '/game',
+        element: <GamePage />,
+      },
+      {
+        path: '/profile',
+        element: <ProfilePage />,
+      },
+      {
+        path: '/edit-profile',
+        element: <EditProfilePage />,
+      },
+      {
+        path: '/register',
+        element: <RegisterPage />,
+      },
+      {
+        path: '/personalize-account',
+        element: <PersonalizeAccountPage />,
+      },
+      {
+        path: '/log-in',
+        element: <LogInPage />,
+      },
+    ],
   },
 ])
