2d566a5144c70c5a5e168f2f41bc4abdb037554a
ell-ska <ella@skagerstrom.se>
2025-06-02 17:09:43 +0200

    fix: responsive

--- apps/frontend/src/pages/Landing.tsx
+++ apps/frontend/src/pages/Landing.tsx
@@ -12,14 +12,19 @@ export const LandingPage = () => {
           Log in
         </Button>
       </header>
-      <main className="flex w-full grow flex-col items-center justify-center bg-[url(/landing-background.svg)] bg-bottom bg-no-repeat 2xl:bg-[100%,auto]">
+      <main className="flex w-full grow flex-col items-center justify-center">
+        <img
+          src="/landing-background.svg"
+          alt=""
+          className="absolute top-3/4 -z-10 h-1/4 w-full object-cover"
+        />
         <div className="flex w-full max-w-3xl grow flex-col items-center justify-end gap-8 px-6 md:gap-0">
           <Logo
             text
             size="lg"
             classname="md:absolute md:top-1/2 md:-translate-y-1/2"
           />
-          <div className="flex w-full justify-between pb-56">
+          <div className="flex w-full justify-between pb-[20vh]">
             <Face
               suit="heart"
               value="jack"
@@ -31,7 +36,7 @@ export const LandingPage = () => {
             as={Link}
             to="/authenticate"
             variant="blue"
-            className="absolute bottom-40 md:bottom-54"
+            className="absolute bottom-1/10 md:bottom-1/5"
           >
             Play now!
           </Button>
