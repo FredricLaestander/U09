06a407d6152d716507ad06d542ff247a3bf233d2
ell-ska <ella@skagerstrom.se>
2025-06-02 10:45:32 +0200

    feat: main menu

--- apps/frontend/src/pages/MainMenu.tsx
+++ apps/frontend/src/pages/MainMenu.tsx
@@ -1,3 +1,23 @@
+import { Link } from 'react-router'
+import { Button } from '../components/Button'
+import { Face } from '../components/Face'
+import { Logo } from '../components/Logo'
+
 export const MainMenuPage = () => {
-  return <div>Main menu page</div>
+  return (
+    <main className="flex h-full grow flex-col items-center justify-center gap-8">
+      <div className="flex items-center gap-4">
+        <Logo text />
+        <Face suit="heart" value="jack" />
+      </div>
+      <nav className="flex w-full flex-col gap-3">
+        <Button as={Link} to="/game" variant="blue">
+          Play
+        </Button>
+        <Button as={Link} to="/profile">
+          Profile
+        </Button>
+      </nav>
+    </main>
+  )
 }
