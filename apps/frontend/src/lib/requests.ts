0bb7b86fabc0415889b2d17fa8878cf77ffb2d5e
ell-ska <ella@skagerstrom.se>
2025-06-03 16:43:38 +0200

    feat: refresh token interceptor

--- apps/frontend/src/lib/requests.ts
+++ apps/frontend/src/lib/requests.ts
@@ -1,5 +1,16 @@
+import axios from 'axios'
 import type { User } from '../types/data'
 import { backend } from './clients/backend'
+
+export const refreshTokens = async () => {
+  // this needs to be it's own client to avoid an infinite loop with the interceptor
+  await axios
+    .create({
+      baseURL: import.meta.env.VITE_BACKEND_URL,
+      withCredentials: true,
+    })
+    .get('/auth/refresh-token')
+}
 
 export const getUser = async () => {
   try {
