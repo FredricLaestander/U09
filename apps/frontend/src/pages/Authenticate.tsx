6a4f2b210428cbcacda0b8050fee2d297135b5e7
ell-ska <ella@skagerstrom.se>
2025-06-03 10:01:31 +0200

    feat: authenticate page

--- apps/frontend/src/pages/Authenticate.tsx
+++ apps/frontend/src/pages/Authenticate.tsx
@@ -1,3 +1,19 @@
+import { Link, useSearchParams } from 'react-router'
+import { Button } from '../components/Button'
+import { Logo } from '../components/Logo'
+
 export const AuthenticatePage = () => {
-  return <div>authenticate page</div>
+  const [searchParams] = useSearchParams()
+  const error = searchParams.get('error')
+
+  return (
+    <main className="flex grow flex-col items-center justify-center gap-6">
+      <Logo text />
+      <Button as={Link} to={`${import.meta.env.VITE_BACKEND_URL}/auth/google`}>
+        <img src="/google.svg" alt="Google logo" />
+        Continue with Google
+      </Button>
+      {error && <span className="text-sm text-red">{error}</span>}
+    </main>
+  )
 }
