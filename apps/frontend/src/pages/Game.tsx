635b4e6c95e6adb8efc5d773086275014cca6a79
ell-ska <ella@skagerstrom.se>
2025-06-10 11:16:19 +0200

    refactor: split GamePage into smaller component

--- apps/frontend/src/pages/Game.tsx
+++ apps/frontend/src/pages/Game.tsx
@@ -14,104 +14,125 @@ import { displayScore } from '../lib/score'
 import { cn } from '../utils/classname'
 
 export const GamePage = () => {
-  const { player, dealer, stand, hit } = useGame()
+  return (
+    <>
+      <GameHeader />
+      <main className="flex w-full grow flex-col items-center px-4 pb-4 md:pb-8">
+        <div className="flex grow flex-col items-center justify-between pt-[12vh] md:pt-8">
+          <DealerHand />
+          <PlayerHand />
+        </div>
+        <Actions />
+      </main>
+      <GameMenu />
+      <GameOver />
+    </>
+  )
+}
+
+const GameHeader = () => {
   const { user } = useAuth()
   const { open } = useModal()
 
   return (
-    <>
-      <Header>
-        <Button onClick={() => open('game-menu')} size="sm">
-          Menu
+    <Header>
+      <Button onClick={() => open('game-menu')} size="sm">
+        Menu
+      </Button>
+      {user.statistics.streak > 1 && (
+        <Statistic illustration="fire" value={String(user.statistics.streak)} />
+      )}
+    </Header>
+  )
+}
+
+const DealerHand = () => {
+  const { dealer } = useGame()
+
+  return (
+    <section className="flex flex-col items-center gap-4">
+      <div className="flex gap-3">
+        {dealer.cards.map(({ id, open, suit, value }) => {
+          if (open) {
+            return <CardFront key={id} suit={suit} value={value} size="sm" />
+          } else {
+            return <CardBack key={id} size="sm" />
+          }
+        })}
+      </div>
+
+      <Count value={displayScore(dealer.score)} />
+    </section>
+  )
+}
+
+const PlayerHand = () => {
+  const { player } = useGame()
+
+  return (
+    <section
+      style={{
+        gridTemplateColumns: Array(player.cards.length)
+          .fill('min-content')
+          .join(' '),
+      }}
+      className="relative grid pb-6 md:pb-0"
+    >
+      {player.cards.map(({ id, suit, value }, index) => (
+        <CardFront
+          key={id}
+          suit={suit}
+          value={value}
+          style={{
+            gridColumn: `${index + 1} / ${index + 3}`,
+          }}
+          classname={cn(
+            'row-1',
+            index % 2 === 0 && '-rotate-2',
+            index % 2 === 1 && 'rotate-2',
+          )}
+          size="md"
+        />
+      ))}
+      <Count
+        value={displayScore(player.score)}
+        classname="absolute top-4 left-[calc(100%_-_2rem)]"
+      />
+    </section>
+  )
+}
+
+const Actions = () => {
+  const { stand, hit } = useGame()
+
+  return (
+    <nav className="flex w-full max-w-sm flex-col gap-3 md:absolute md:top-1/2 md:left-1/2 md:-translate-1/2">
+      <Button
+        variant="blue"
+        size="sm"
+        className="top-1/2 -translate-y-1/2 self-end md:absolute md:left-[calc(100%_+_0.75rem)]"
+      >
+        Split
+      </Button>
+
+      <div className="flex gap-3">
+        <Button
+          onClick={hit}
+          variant="green"
+          icon={Plus}
+          className="flex-1 md:w-36"
+        >
+          Hit
         </Button>
-        {user.statistics.streak > 1 && (
-          <Statistic
-            illustration="fire"
-            value={String(user.statistics.streak)}
-          />
-        )}
-      </Header>
-
-      <main className="flex w-full grow flex-col items-center px-4 pb-4 md:pb-8">
-        <div className="flex grow flex-col items-center justify-between pt-[12vh] md:pt-8">
-          <section className="flex flex-col items-center gap-4">
-            <div className="flex gap-3">
-              {dealer.cards.map(({ id, open, suit, value }) => {
-                if (open) {
-                  return (
-                    <CardFront key={id} suit={suit} value={value} size="sm" />
-                  )
-                } else {
-                  return <CardBack key={id} size="sm" />
-                }
-              })}
-            </div>
-
-            <Count value={displayScore(dealer.score)} />
-          </section>
-          <section
-            style={{
-              gridTemplateColumns: Array(player.cards.length)
-                .fill('min-content')
-                .join(' '),
-            }}
-            className="relative grid pb-6 md:pb-0"
-          >
-            {player.cards.map(({ id, suit, value }, index) => (
-              <CardFront
-                key={id}
-                suit={suit}
-                value={value}
-                style={{
-                  gridColumn: `${index + 1} / ${index + 3}`,
-                }}
-                classname={cn(
-                  'row-1',
-                  index % 2 === 0 && '-rotate-2',
-                  index % 2 === 1 && 'rotate-2',
-                )}
-                size="md"
-              />
-            ))}
-            <Count
-              value={displayScore(player.score)}
-              classname="absolute top-4 left-[calc(100%_-_2rem)]"
-            />
-          </section>
-        </div>
-
-        <nav className="flex w-full max-w-sm flex-col gap-3 md:absolute md:top-1/2 md:left-1/2 md:-translate-1/2">
-          <Button
-            variant="blue"
-            size="sm"
-            className="top-1/2 -translate-y-1/2 self-end md:absolute md:left-[calc(100%_+_0.75rem)]"
-          >
-            Split
-          </Button>
-
-          <div className="flex gap-3">
-            <Button
-              onClick={hit}
-              variant="green"
-              icon={Plus}
-              className="flex-1 md:w-36"
-            >
-              Hit
-            </Button>
-            <Button
-              onClick={stand}
-              variant="red"
-              icon={X}
-              className="flex-1 md:w-36"
-            >
-              Stand
-            </Button>
-          </div>
-        </nav>
-      </main>
-
-      <GameMenu />
-      <GameOver />
-    </>
+        <Button
+          onClick={stand}
+          variant="red"
+          icon={X}
+          className="flex-1 md:w-36"
+        >
+          Stand
+        </Button>
+      </div>
+    </nav>
   )
 }
