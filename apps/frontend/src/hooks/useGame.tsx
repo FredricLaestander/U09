2f7fecbf0cc19ed576fe93c0cfaa66b44824b770
ell-ska <ella@skagerstrom.se>
2025-06-11 09:28:24 +0200

    feat: player bust

--- apps/frontend/src/hooks/useGame.tsx
+++ apps/frontend/src/hooks/useGame.tsx
@@ -91,6 +91,11 @@ export const GameProvider = ({ children }: { children: ReactNode }) => {
     const cards = [...player.cards, card]
     const score = calculateScore(cards)
     setPlayer({ cards, score })
+
+    if (score.hard > 21) {
+      await sleep(1000)
+      setTurn('over')
+    }
   }
 
   return (
