70379db88aab2b1625b2ba81782ed897aafe5d56
ell-ska <ella@skagerstrom.se>
2025-06-16 14:45:29 +0200

    refactor: initialize hono

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -1,24 +1,32 @@
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-import express from 'express'
-import helmet from 'helmet'
-import { router } from './routes'
+// import cookieParser from 'cookie-parser'
+// import cors from 'cors'
+// import express from 'express'
+// import helmet from 'helmet'
+import { Hono } from 'hono'
 
-const app = express()
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  }),
-)
-app.use(helmet())
-app.use(express.json())
-app.use(cookieParser())
+// import { router } from './routes'
 
-app.use(router)
+// const app = express()
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   }),
+// )
+// app.use(helmet())
+// app.use(express.json())
+// app.use(cookieParser())
 
-app.listen(process.env.PORT, () => {
-  console.log(`server ready on http://localhost:${process.env.PORT}`)
+// app.use(router)
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`server ready on http://localhost:${process.env.PORT}`)
+// })
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.json({ message: 'Hello Hono!' })
 })
 
 export default app
